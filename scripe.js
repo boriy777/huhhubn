4faa7ba16f47e4e89e6c48f8cfdcdd55f81ad386
boriy777 <abdulboriy822@gmail.com>
2024-11-01 17:35:59 +0500

    Add files via upload

---
+++ scripe.js
@@ -0,0 +1,27 @@
+let a = prompt('ismingizni kiriting')
+let b = prompt('id kiriting')
+let c = prompt('parolingizni kiriting')
+
+
+
+let data = {
+
+    name:'abdulboriy',
+
+    id: 123,
+    
+    parol: 1234,
+
+
+    name: a,
+
+    id:  b,
+
+    parol: c,
+}
+
+
+
+
+
+console.log(data);
